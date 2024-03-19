2941f419c43fcb22522c4838b89a0f31352b279b
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-19 22:47:43 +0530

    maxChar - base solution

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-//* Given a string, return the character that has the most no. of occurrences in the string
-// maxChar("Steve Reese") -> 'e'
-// maxChar("12341111") -> '1'
+console.log('Hello World!');
--- problems/03-max-repeated-character.js
+++ problems/03-max-repeated-character.js
@@ -1,3 +1,29 @@
 //* Given a string, return the character that has the most no. of occurrences in the string
 // maxChar("Steve Reese") -> 'e'
 // maxChar("12341111") -> '1'
+
+const maxChar = (str) => {
+  let strCount = {};
+  let maxCountKey = null;
+  let maxCountValue = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const curChar = str[i];
+
+    if (curChar in strCount) {
+      strCount[curChar] = strCount[curChar] + 1;
+    } else {
+      strCount[curChar] = 1;
+    }
+
+    if (strCount[curChar] > maxCountValue) {
+      maxCountKey = curChar;
+      maxCountValue = strCount[curChar];
+    }
+  }
+
+  return maxCountKey;
+};
+
+const value = maxChar('abbccccddd');
+console.log(value);
