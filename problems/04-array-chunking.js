b29f3c3136bbfa0f10ab73b9d55abce8857a3771
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-20 19:58:40 +0530

    array chunking - base solution

--- problems/04-array-chunking.js
+++ problems/04-array-chunking.js
@@ -6,4 +6,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunkedArray = [];
+  let curChunk = [];
+
+  for (let i = 0; i < array.length; i++) {
+    curChunk.push(array[i]);
+
+    if (curChunk.length === size) {
+      chunkedArray.push(curChunk);
+      curChunk = [];
+    }
+  }
+
+  curChunk.length > 0 && chunkedArray.push(curChunk);
+
+  return chunkedArray;
+}
+
+const result = chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
+console.log(result);
