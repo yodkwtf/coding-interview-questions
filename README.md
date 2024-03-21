1a79af8a7d5eeb7025ced72306d108e6fd3caadc
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-21 20:12:57 +0530

    anagrams - problem statement

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ This repository contains solutions to some common coding interview problems. The
 |  3  | Max Repeated Character      | [Solution](./problems/03-max-repeated-character.js) | Strings, Hashmaps |
 |  4  | Array Chunking              | [Solution](./problems/04-array-chunking.js)         | Arrays            |
 |  5  | Capitalize String           | [Solution](./problems/05-capiatalize-string.js)     | Strings, Arrays   |
+|  6  | Anagrams                    | [Solution](./problems/06-anangrams.js)              | Strings           |
---
+++ problems/06-anangrams.js
@@ -0,0 +1,12 @@
+//* Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters in the same quantity. Only consider characters, not spaces or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('coding money', 'money coding') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+const anagrams = (str1, str2) => {};
+
+// - Function Call
+const result = anagrams('RAIL! SAFETY!', 'fairy tales');
+console.log(result);
