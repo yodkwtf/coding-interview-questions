0c007fcce7795164fe8a6ae01b3d5b335e91e221
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-23 01:56:34 +0530

    vowel count - solution

--- problems/07-vowel-count.js
+++ problems/07-vowel-count.js
@@ -5,7 +5,16 @@
 //   vowels('Coding Money') --> 4
 //   vowels('why?') --> 0
 
-const vowels = (str) => {};
+const vowels = (str) => {
+  const vowelList = ['a', 'e', 'i', 'o', 'u'];
+  let vowelCount = 0;
+
+  for (let char of str.toLowerCase()) {
+    vowelList.includes(char) && vowelCount++;
+  }
+
+  return vowelCount;
+};
 
 // - Function Call
 const result = vowels('How are yoU?');
