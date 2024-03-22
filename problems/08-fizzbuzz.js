38e2f5fedecb429c387bf00235e1aa528744428e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-23 02:45:57 +0530

    fizzbuzz - multiple solutions

--- problems/08-fizzbuzz.js
+++ problems/08-fizzbuzz.js
@@ -9,6 +9,63 @@
  * buzz
  */
 
-const fizzbuzz = (n) => {};
+//# For loop with switch statements
+const fizzbuzz = (n) => {
+  for (let count = 1; count <= n; count++) {
+    switch (true) {
+      case count % 3 === 0 && count % 5 === 0:
+        console.log('fizzbuzz');
+        break;
+      case count % 3 === 0:
+        console.log('fizz');
+        break;
+      case count % 5 === 0:
+        console.log('buzz');
+        break;
+      default:
+        console.log(count);
+    }
+  }
+};
 
-fizzbuzz(5);
+//# While loop
+// const fizzbuzz = (n) => {
+//   let counter = 1;
+
+//   while (counter <= n) {
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (counter % 3 === 0) {
+//       console.log('fizz');
+//     } else if (counter % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(counter);
+//     }
+//     counter++;
+//   }
+// };
+
+//# For loop
+// const fizzbuzz = (n) => {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//       continue;
+//     }
+
+//     if (i % 3 === 0) {
+//       console.log('fizz');
+//       continue;
+//     }
+
+//     if (i % 5 === 0) {
+//       console.log('buzz');
+//       continue;
+//     }
+
+//     console.log(i);
+//   }
+// };
+
+fizzbuzz(20);
