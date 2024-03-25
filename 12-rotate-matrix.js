90087be9ed223672774e9834c11ea91c3962fd2e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-03-25 22:04:35 +0530

    rotate matrix - problem statement

---
+++ 12-rotate-matrix.js
@@ -0,0 +1,56 @@
+//* Write a function that accepts a square matrix of size N x N and rotates it 90 degrees clockwise.
+// --- Examples
+//   rotateMatrix([
+//     [1, 2],
+//     [3, 4]
+//   ])
+//     returns [
+//       [3, 1],
+//       [4, 2]
+//     ]
+//   rotateMatrix([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ])
+//     returns [
+//       [7, 4, 1],
+//       [8, 5, 2],
+//       [9, 6, 3]
+//     ]
+//  rotateMatrix([
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16]
+//   ])
+//     returns [
+//       [13, 9, 5, 1],
+//       [14, 10, 6, 2],
+//       [15, 11, 7, 3],
+//       [16, 12, 8, 4]
+//     ]
+
+const rotateMatrix = (matrix) => {};
+
+//# Function call
+const mat2 = [
+  [1, 2],
+  [3, 4],
+];
+
+const mat3 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const mat4 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+const result = rotateMatrix(mat2);
+console.log(result);
